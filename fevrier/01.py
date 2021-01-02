0499484d7d65484306ee27abf08094dfbb3ea441
rene-d <rene-d@users.noreply.github.com>
2021-01-02 11:25:45 +0100

    plus simple

--- fevrier/01.py
+++ fevrier/01.py
@@ -2,17 +2,7 @@
 
 # Lundi 1 Février
 
-from itertools import combinations
-from math import prod
-
-# les diviseurs de 1260
-divisors = (2, 2, 3, 3, 5, 7)
-
-m = 0
-for r in range(len(divisors)):
-    for d in combinations(divisors, r + 1):
-        p = prod(d)
-        if p < 100 and m < p:
-            m = p
+# tous les diviseurs qui sont inférieurs à 100
+m = max(d for d in range(1, 1260 // 2 + 1) if 1260 % d == 0 and d < 100)
 
 print("réponse:", m)
