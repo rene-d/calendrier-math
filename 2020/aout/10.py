aae22ec5986f9bf881103b0eff42a5a8dcf70a8b
rene-d <rene-d@users.noreply.github.com>
2021-02-06 09:03:37 +0100

    commentaires

--- 2020/aout/10.py
+++ 2020/aout/10.py
@@ -2,18 +2,44 @@
 
 from fractions import Fraction
 
-des = [[0, 0, 4, 4, 4, 4], [3, 3, 3, 3, 3, 3], [2, 2, 2, 2, 6, 6], [1, 1, 1, 5, 5, 5]]
+des = [
+    [0, 0, 4, 4, 4, 4],
+    [3, 3, 3, 3, 3, 3],
+    [2, 2, 2, 2, 6, 6],
+    [1, 1, 1, 5, 5, 5],
+]
 
-nb = 0
-lea_gagne = 0
+total_nb = 0
+total_lea_gagne = 0
 
+# Jean choisit un dé
 for de_jean in range(4):
-    for tirage_jean in des[de_jean]:
-        for de_lea in range(4):
-            if de_lea != de_jean:
-                for tirage_lea in des[de_lea]:
-                    nb += 1
-                    if tirage_lea > tirage_jean:
-                        lea_gagne += 1
 
-print(lea_gagne, nb, Fraction(lea_gagne, nb))
+    des_lea = set(range(4))
+    des_lea.remove(de_jean)
+
+    nb = 0
+    lea_gagne = 0
+
+    # Léa choisit un dé parmi les trois restants
+    for de_lea in des_lea:
+
+        # Jean tire son dé
+        for tirage_jean in des[de_jean]:
+
+            # Léa tire son dé
+            for tirage_lea in des[de_lea]:
+                nb += 1
+
+                # Léa gagne si son tirage est supérieur à celui de Jean
+                if tirage_lea > tirage_jean:
+                    lea_gagne += 1
+
+    f = Fraction(lea_gagne, nb)
+    print(f"de_jean: {des[de_jean]} lea_gagne: {lea_gagne}/{nb} = {f}")
+
+    total_lea_gagne += lea_gagne
+    total_nb += nb
+
+f = Fraction(total_lea_gagne, total_nb)
+print(f"total_lea_gagne: {total_lea_gagne}/{total_nb} = {f}")
